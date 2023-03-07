6b1e9ffe55cb1581c961ac18b9d4ccf1cf15bee4
badr151 <badrkahouaji26@gmail.com>
2023-03-07 15:59:13 +0100

    create employee list

--- src/main/java/com/example/Employees/Controller/EmployeeController.java
+++ src/main/java/com/example/Employees/Controller/EmployeeController.java
@@ -1,14 +1,22 @@
 package com.example.Employees.Controller;
 
+import com.example.Employees.models.Employee;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/Api")
 @CrossOrigin("*")
 public class EmployeeController {
+
+    List<Employee> employeeList = new ArrayList<>(
+            List.of(new Employee("badr","badrkahouaji26@gmail.com","badr151")
+            ,new Employee("aneeb","aneebch@gmail.com","aneebch")));
     @GetMapping("/employee")
     public String employee(){
         System.out.println("good");
--- src/main/java/com/example/Employees/models/Employee.java
+++ src/main/java/com/example/Employees/models/Employee.java
@@ -54,3 +54,4 @@ public class Employee {
         this.username = username;
     }
 }
+
