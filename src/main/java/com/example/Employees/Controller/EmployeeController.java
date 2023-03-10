e01f595c69a51381ba844697db2fadf08b82c05f
Aneebch <aneeb.chatha786@gmail.com>
2023-03-10 09:34:50 +0100

    Cross origin

--- src/main/java/com/example/Employees/Controller/EmployeeController.java
+++ src/main/java/com/example/Employees/Controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/Api")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class EmployeeController {
 
     List<Employee> employeeList = new ArrayList<>(
